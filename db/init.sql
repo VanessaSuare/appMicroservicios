cfa3db14c38ec40a2f43062d44cf2815bf9fba5d
VanessaSuare <dayanna.suarez@uao.edu.co>
2023-04-19 17:23:19 -0500

    first commit

---
+++ db/init.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE almacen2;
+use almacen2;
+
+CREATE TABLE usuarios (
+	nombre varchar (50), 
+	email varchar (50), 
+	usuario varchar (20), 
+	password varchar (20), 
+	admin boolean, 
+	primary key(usuario));
+
+CREATE TABLE ordenes(
+	id int auto_increment, 
+	nombreCliente varchar(50), 
+	emailCliente varchar(50), 
+	totalCuenta decimal(10,2), 
+	fecha datetime default CURRENT_TIMESTAMP, 
+	primary key (id));
+
+CREATE TABLE productos(
+	id int auto_increment, 
+	nombre varchar (50), 
+	precio decimal (10,2), 
+	inventario int, 
+	primary key(id));
+
+insert into usuarios values("Vanessa", "vanessa@gmail.com", "Nessa", "123456", False);
+insert into usuarios values("Paola", "paola@gmail.com", "Pao", "123456", True);
+insert into productos values(null, "Pollo Asado", 10000, 59);
+insert into productos values(null, "Celular", 8000, 296);
+insert into productos values(null, "Laptop", 6500, 485);
+
